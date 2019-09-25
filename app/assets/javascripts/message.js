a67b4312bafcd9868b9ef8f9833361de159c3c81
mitsuihiro <hiromitsui576@gmail.com>
2019-09-25 12:40:12 +0900

    reset();追加

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,6 +1,5 @@
 $(function(){
   function buildHTML(message){
-    console.log(message)
     image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image} >` : "";
     var html = `<div class="content">
                   <div class="main-content-message">
@@ -25,9 +24,8 @@ $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     var formData = new FormData(this); 
-    
     var url = $(this).attr('action')
-    console.log(url)
+    
     $.ajax({
       url: url,
       type: "POST",
@@ -36,11 +34,10 @@ $(function(){
       processData: false,
       contentType: false
     }) 
-    .done(function(data){
-      
+    .done(function(data){      
       var html = buildHTML(data);
-      console.log(html)
       $('.contents').append(html)
+      $('.new_message')[0].reset();
       $('#message_content.form__message').val('')
       $('.contents').animate({scrollTop: $('.contents')[0].scrollHeight}, 'fast');
     })
