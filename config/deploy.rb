ededd51352b4364c352bba367a07de572d2739e2
mitsuihiro <hiromitsui576@gmail.com>
2019-10-31 20:46:03 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 set :ssh_options, auth_methods: ['publickey'],
-                  keys: ['~/.ssh/h-a-mkey.pem']  
+                  keys: ['~/.ssh/h-a-mkey_pem.pem']  
 set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
