7623456ea6fe4e9ac8c1a0fb0933d4172728c90d
mitsuihiro <hiromitsui576@gmail.com>
2019-11-05 20:36:50 +0900

    修正

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :rbenv_type, :user
 set :rbenv_ruby, '2.5.1' 
 set :ssh_options, auth_methods: ['publickey'],
                   keys: ['~/.ssh/h-a-mkey.pem']  
-set :unicorn_pid, -> { "/var/www/chat-space/tmp/pids/unicorn.pid" }
+set :unicorn_pid, -> { "#{shared_path}/tmp/pids/unicorn.pid" }
 set :unicorn_config_path, -> { "#{current_path}/config/unicorn.rb" }
 set :keep_releases, 5
 
