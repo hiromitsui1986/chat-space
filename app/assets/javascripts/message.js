e34505461b872a8f7a82c4f7b59f741b368cbaf8
mitsuihiro <hiromitsui576@gmail.com>
2019-11-17 18:44:33 +0900

    修正

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,4 @@
-$(document).on("turbolinks:load", function () {
+$(function() {
   function buildHTML(message){
     image = ( message.image ) ? `<img class= "lower-message__image" src=${message.image} >` : "";
     var html = `<div class="content" data-message-id="${message.id}"> 
